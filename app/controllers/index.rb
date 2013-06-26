ed7bd3267d98fc6a62a21ec42b94a8b8547cdc4d
DBC-Apprentice <dbc@dbc.com>
2013-06-26 12:24:32 -0700

    finished lucky ajax challenge

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 get '/' do
   erb :index
 end
@@ -5,11 +7,8 @@ end
 
 # TODO: convert this route to use AJAX
 post '/rolls' do
-  # If the user passes-in a "value", let's use it. Otherwise, we'll generate a random one.
-  # See: roll_if_value_is_nil method in the Roll model.
-  value = params[:value] ? params[:value].to_i : nil
-
+  value = params[:value] != "" ? params[:value].to_i : nil
   @roll = value ? Roll.create({ value: value }) : Roll.create
-
-  erb :index  # HINT: what does this do? what should we do instead?
+  @roll.to_json
+  # erb :index  # HINT: what does this do? what should we do instead?
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
   // PSEUDO-CODE:
   //   1- intercept the form submission event using jQuery
@@ -6,5 +6,19 @@ $(document).ready(function () {
   //   3- generate a random number between 1 and 6 using JavaScript
   //   4- use jQuery to submit an AJAX post to the form's action
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
+
+  $('#dice_form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: '/rolls',
+      type: 'post',
+      data: $('#input').serialize(),
+      dataType: 'json'
+    }).done(function(data){
+      var value = data.roll.value;
+      $('#die').find('img').remove();
+      $('#die').append('<img src=/' + value + '.png title=' + value + ' alt="the roll">');
+    });
+  });
 
 });
